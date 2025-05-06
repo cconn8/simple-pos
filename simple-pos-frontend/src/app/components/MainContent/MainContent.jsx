24f8c96ff22b6b11bbead84e1b1e9512264e55f2
Colm <colm.conneely@paack.co>
2025-05-06 23:08:20 +0100

    rendering item data to sidebar

--- simple-pos-frontend/src/app/components/MainContent/MainContent.jsx
+++ simple-pos-frontend/src/app/components/MainContent/MainContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import InformationContainer from "../InformationConatiner/InformationContainer";
 import ProductsAndServicesContainer from "../ProductsAndServicesContainer/ProductsAndServicesContainer";
 
-export default function MainContent() {
+export default function MainContent({selectedItems, setSelectedItems}) {
     const informationCategories = [
         {
         title : "Deceased Details",
@@ -35,7 +35,6 @@ export default function MainContent() {
           }]
         }
       ]
-
       const productAndServicesCategories = [
         {
           title : "Coffin",
@@ -97,11 +96,10 @@ export default function MainContent() {
       ]
 
     return(
-        <>
+        <div id="main-content-div">
             <InformationContainer categories={informationCategories}/>
-            <ProductsAndServicesContainer categories={productAndServicesCategories} />
-            {/* <SideBar /> */}
-        </>
+            <ProductsAndServicesContainer categories={productAndServicesCategories} selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
+        </div>
     )
 
 }
--- simple-pos-frontend/src/app/components/ProductsAndServicesContainer/ProductsAndServicesContainer.jsx
+++ simple-pos-frontend/src/app/components/ProductsAndServicesContainer/ProductsAndServicesContainer.jsx
@@ -1,30 +1,35 @@
 'use client';
-import React from "react";
+import React, { useState } from "react";
+import SideBar from "../InformationConatiner/SideBar";
 
-export default function ProductsAndServicesContainer({categories}) {
+export default function ProductsAndServicesContainer({categories, selectedItems, setSelectedItems}) {
 
-    function handleClick() {
-        console.log('Button clicked!');
-      };
+    function onItemClick(item) {
+        console.log('Item Button clicked!');
+        setSelectedItems( (prev) => [...prev, item]);
+        };
 
     return (
         <div id="products-and-services-container">
 
+            <h1 className="my-5 underline"><b>Products & Services</b></h1>
+
             {categories.map( (category) => (
                 
-                <div className="grid" key={category.title} id='product-category-container'>
+                <div className="my-4 p-4 border" key={category.title} id='product-category-container'>
                     <h2>{category.title}</h2>
-                    <div className="border rounded-sm p-5 shadow-md gap-2" id="product-items-container">
-                        {category.items.map( (item) => (
-                                <button 
-                                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 m-2" 
-                                    onClick={handleClick}>
+                    <div className="p-5 shadow-md gap-2" id="product-items-container">
+                        {category.items.map( (item, index) => (
+                                <button  key={item.title + '' + index}
+                                    className="bg-blue-500 text-white p-10 rounded hover:bg-blue-600 m-1" 
+                                    onClick={ () => onItemClick(item)}>
                                         {item.title}
                                 </button>
                         ))}
+                        <button className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-red-600 m-2"> + Add Item</button>
                     </div>
                 </div>          
-            ))}
+            ))}            
         </div>
     )
 }
