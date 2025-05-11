27fb90de36351965ff93e5347afe6b159a3df77f
Colm <colm.conneely@paack.co>
2025-05-11 20:16:43 +0100

    working sidebar render

--- simple-pos-frontend/src/app/components/MainContent/MainContent.jsx
+++ simple-pos-frontend/src/app/components/MainContent/MainContent.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import InformationContainer from "../InformationConatiner/InformationContainer";
 import ProductsAndServicesContainer from "../ProductsAndServicesContainer/ProductsAndServicesContainer";
 
-export default function MainContent({informationCategories, productAndServicesCategories, selectedItems, setSelectedItems}) {
+export default function MainContent({informationCategories, productAndServicesCategories, selectedItems, setSelectedItems, formData, setFormData}) {
 
     return(
         <div id="main-content-div">
-            <InformationContainer categories={informationCategories}/>
+            <InformationContainer categories={informationCategories} formData={formData} setFormData={setFormData}/>
             <ProductsAndServicesContainer categories={productAndServicesCategories} selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
         </div>
     )
--- simple-pos-frontend/src/app/components/SideBar/SideBar.jsx
+++ simple-pos-frontend/src/app/components/SideBar/SideBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function SideBar({setSelectedItems, selectedItems}) {
+export default function SideBar({setSelectedItems, selectedItems, formData, setFormData}) {
 
     function onDeleteSelectedItemButtonClick(id) {
         setSelectedItems( (prevItems) => 
@@ -9,28 +9,37 @@ export default function SideBar({setSelectedItems, selectedItems}) {
     }
 
     function onClearAllButtonClick() {
-        setSelectedItems()
+        setSelectedItems([])
     }
     
     return(
         
-        <div id="sidebar-container">
+        <div id="sidebar-container" className="p-5 shadow-md bg-gray-200">
 
+            <h1 className="my-5 underline"><b>Funeral Summary</b></h1>
+            <div className="flex flex-row">
+                <label>Deceased : {formData.deceased_name}</label>
+            </div>
+
+            <h1> Funeral of the late : {formData.deceased_name}</h1>
+            
             <h1 className="my-5 underline"><b>Selected Items</b></h1>
 
             <div className="mx-2 my-4 p-4 border" id="selected-items-div">
 
-                {selectedItems ? selectedItems.map( (item, index) => (
+                {selectedItems?.length > 0 ? (
+                    selectedItems.map( (item, index) => (
                         
-                        <div key={item.id || index} className="flex flex-row border">
+                        <div key={item.id || index} className="flex flex-row border my-1">
                             <h3 className="mx-1">{item.title} : </h3>
                             <p className="mx-1">{item.currency}{item.price}</p>
                             <button className="mx-1 underline" onClick={( () => onDeleteSelectedItemButtonClick(item.id))}>Delete</button>
                             {console.log('Sidebar item:', item)}
                         </div>
                         )
-                    ) : 'No items selected'
-                }
+                    )) : 
+                        <div><h3>No items selected</h3></div>
+            }
 
                 <div id="sidebar-buttons-dive" className="my-2">
                     <button className="mx-2 underline">Save</button>
