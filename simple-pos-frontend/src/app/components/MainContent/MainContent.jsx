88ba71743882d4c4e976dcf6248ff476fcde79f1
Colm <colm.conneely@paack.co>
2025-05-16 14:50:10 +0100

    updates

--- simple-pos-frontend/src/app/components/MainContent/MainContent.jsx
+++ simple-pos-frontend/src/app/components/MainContent/MainContent.jsx
@@ -1,16 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import InformationContainer from "../InformationConatiner/InformationContainer";
-import ProductsAndServicesContainer from "../ProductsAndServicesContainer/ProductsAndServicesContainer";
-
-export default function MainContent({informationCategories, productAndServicesCategories, selectedItems, setSelectedItems}) {
-
-    return(
-        <div id="main-content-div">
-            <InformationContainer categories={informationCategories}/>
-            <ProductsAndServicesContainer categories={productAndServicesCategories} selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
-        </div>
-=======
 
 export default function MainContent({categories, formData, setFormData, selectedItems, setSelectedItems}) {
 
