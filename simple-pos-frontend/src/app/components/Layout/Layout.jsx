a8e2dfe48f7208e832fc462ac3686774c6437033
Colm <colm.conneely@paack.co>
2025-05-18 21:28:00 +0100

    invoicing template

--- simple-pos-frontend/src/app/components/Layout/Layout.jsx
+++ simple-pos-frontend/src/app/components/Layout/Layout.jsx
@@ -3,19 +3,8 @@ import React, { useState } from "react";
 import MainContent from "../MainContent/MainContent";
 import SideBar from "../SideBar/SideBar";
 import { v4 as uuidv4 } from 'uuid';
+import Invoice from "../Invoice/Invoice";
 
-<<<<<<< HEAD
-
-export default function Layout() {
-    const [selectedItems, setSelectedItems] = useState([])
-    const informationCategories = [
-        {
-        title : "Deceased Details",
-        id: uuidv4(),
-          fields : [{
-            id : uuidv4(),
-            title : "DeceasedName",
-=======
 const categories = [
       {
         name: 'deceased_details',
@@ -26,22 +15,15 @@ const categories = [
             id : uuidv4(),
             name : "deceased_name",
             display_text: "Deceased Name",
->>>>>>> d56167b0c28d72597d74c712d25737eba8b53eec
             type : "text"
           },
           {
             id : uuidv4(),
-<<<<<<< HEAD
-            title : "DateOfDeath",
-=======
-            name: "deceased_date_of_death",
-            display_text : "Date of Death",
->>>>>>> d56167b0c28d72597d74c712d25737eba8b53eec
+            title : "deceased_date_of_death",
             type : "date"
           },
           {
             id : uuidv4(),
-<<<<<<< HEAD
             title : "LastAddress",
             type : "text"
           }]
@@ -52,181 +34,15 @@ const categories = [
           fields : [{
             id : uuidv4(),
             title : "ClientName",
-=======
-            name: "deceased_last_address",
-            display_text : "Last Address",
-            type : "text"
-          }]},
-      {
-        name: "client_details",
-        id: uuidv4(),
-        type: 'information',
-        display_text : "Client Details",
-        fields : [{
-            id : uuidv4(),
-            name : "client_name",
-            display_text : "Client Name",
->>>>>>> d56167b0c28d72597d74c712d25737eba8b53eec
             type : "text"
           },
           {
             id : uuidv4(),
-<<<<<<< HEAD
             title : "BillingAddress",
-=======
-            name : "client_billing_address",
-            display_text : "Billing Address",
->>>>>>> d56167b0c28d72597d74c712d25737eba8b53eec
             type : "text"
           },
           {
             id : uuidv4(),
-<<<<<<< HEAD
-            title : "Phone",
-            type : "number"
-          }]
-        }
-      ]
-    const productAndServicesCategories = [
-      {
-        title : "Coffin",
-        id: uuidv4(),
-        items : [
-          {
-            id : uuidv4(),
-            title : "Light Leinster",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "Dark Leinster",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "Timber Leinster",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "Rustic Oak",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "The Kilmacduagh",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "The Galwegian",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "Wax Macroom",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "Wicker Basket",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "Solid Oak Antique",
-            price : 250,
-            currency : "€",
-            description : ""
-          }]
-      },
-      {
-        title : "Flowers",
-        id: uuidv4(),
-        items : [
-          {
-            id : uuidv4(),
-            title : "Fresh Floral Arrangements",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "Standard Coffin Spray",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "5 x Red Roses",
-            price : 250,
-            currency : "€",
-            description : ""
-          }]
-      },
-      {
-        title : "Music",
-        id: uuidv4(),
-        items : [
-          {
-            id : uuidv4(),
-            title : "Carla & Carmel",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "Frankie Colohan",
-            price : 250,
-            currency : "€",
-            description : ""
-          },
-          {
-            id : uuidv4(),
-            title : "Ailbhe Hession",
-            price : 250,
-            currency : "€",
-            description : ""
-          }]
-      },
-    ]
-
-    return(
-        <div className="mx-4 pd-2 flex">
-            <div className="basis-2/3"> 
-                <MainContent 
-                    informationCategories={informationCategories}
-                    productAndServicesCategories={productAndServicesCategories}
-                    selectedItems={selectedItems} 
-                    setSelectedItems={setSelectedItems}
-                />
-            </div>
-            <div className="basis-1/3"> 
-                <SideBar 
-                    selectedItems={selectedItems}
-                    setSelectedItems={setSelectedItems}
-                />
-            </div>
-=======
             name : "client_phone_number",
             display_text: "Phone Number",
             type : "integer"
@@ -364,26 +180,26 @@ export default function Layout() {
     const [formData, setFormData] = useState({})
 
     return(
-          <div className=" pd-2 flex basis-1/2">
-                <MainContent 
-                    categories={categories} 
-                    formData={formData} 
-                    setFormData={setFormData} 
-                    selectedItems={selectedItems} 
-                    setSelectedItems={setSelectedItems}
-                />        
+        <Invoice />
+        //   <div className=" pd-2 flex basis-1/2">
+        //         <MainContent 
+        //             categories={categories} 
+        //             formData={formData} 
+        //             setFormData={setFormData} 
+        //             selectedItems={selectedItems} 
+        //             setSelectedItems={setSelectedItems}
+        //         />        
 
-                <div id="sidebar-container" className="flex basis-1/2 p-5 shadow-md bg-gray-200"> 
-                    <aside className="h-screen sticky top-0 overflow-y-auto">
-                      <SideBar 
-                          selectedItems={selectedItems}
-                          setSelectedItems={setSelectedItems}
-                          formData={formData}
-                          setFormData={setFormData}
-                      />
-                    </aside>
-                </div>
->>>>>>> d56167b0c28d72597d74c712d25737eba8b53eec
-        </div>
+        //         <div id="sidebar-container" className="flex basis-1/2 p-5 shadow-md bg-gray-200"> 
+        //             <aside className="h-screen sticky top-0 overflow-y-auto">
+        //               <SideBar 
+        //                   selectedItems={selectedItems}
+        //                   setSelectedItems={setSelectedItems}
+        //                   formData={formData}
+        //                   setFormData={setFormData}
+        //               />
+        //             </aside>
+        //         </div>
+        // </div>
     )
 }
--- simple-pos-frontend/src/app/page.js
+++ simple-pos-frontend/src/app/page.js
@@ -1,45 +1,8 @@
-import Image from "next/image";
-<<<<<<< HEAD
-import InformationContainer from "./components/InformationConatiner/InformationContainer";
-=======
->>>>>>> d56167b0c28d72597d74c712d25737eba8b53eec
+
 import MainContent from "./components/MainContent/MainContent";
 import Layout from "./components/Layout/Layout";
 
-const informationCategories = [
-  {
-    name : "Deceased Details",
-    fields : [{
-      name : "DeceasedName",
-      type : "text"
-    },
-    {
-      name : "DateOfDeath",
-      type : "date"
-    },
-    {
-      name : "LastAddress",
-      type : "text"
-    }]
-  },
-  {
-    name : "Client Details",
-    fields : [{
-      name : "ClientName",
-      type : "text"
-    },
-    {
-      name : "BillingAddress",
-      type : "text"
-    },
-    {
-      name : "Phone",
-      type : "number"
-    }]
-  }
-]
-
-
+import './index.css'
 
 const styles = {
   margin : 'auto',
@@ -48,6 +11,65 @@ const styles = {
 
 export default function Home() {
   return (
-    <Layout />
+    <main className="m-5 p-5 md:max-w-xl lg:max-w-auto bg-white">
+      <header className="border flex-column justify-center mb-5">
+      {/* Header */}
+          <h2 className="font-bold uppercase tracking-wide text-4xl mb-3 text-center">O'Flaherty Logo</h2>
+          <div id="buttons-div" className="flex justify-center">
+                  <button className="btn btn-print m-1 underline">Print</button>
+                  <button className="btn btn-download m-1 underline">Download</button>
+                  <button className="btn btn-send m-1 underline">Send</button>
+          </div>
+      </header>
+      {/*End of Header */}
+
+      {/* Company Details */}
+      <section className="flex flex-col items-end m-2 ">
+        <h2 className="text-xl uppercase">Company Name</h2>
+        <p>Company Address</p>
+      </section>
+      {/* End of Company Details */}
+
+      {/* Client Details */}
+      <section className="">
+        <h2>Client Name</h2>
+        <p>Client Address</p>
+      </section>
+      {/* End of Client Details */}
+
+
+      {/* Dates */}
+      <section className="flex flex-col items-end my-5  ">
+        <ul>
+          <li>Invoice Number</li>
+          <li>Invoice Date</li>
+          <li>Due Date</li>
+        </ul>
+      </section>
+      {/* ENd of Dates */}
+
+
+      {/* Table Here */}
+
+      {/* Notes */}
+      <section>
+        {/* Textarea */}
+        <p>Notes..</p>
+      </section>
+      {/* End of Notes */}
+
+
+      <footer>
+        <ul>
+          <li>Company Name</li>
+          <li>Company Email</li>
+          <li>Company Phone</li>
+          <li>Bank Name</li>
+          <li>Bank Account</li>
+        </ul>
+      </footer>
+
+
+    </main>
   );
 }
