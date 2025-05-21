a57475154f1979cb8688d3fa8e2043f0f1defc96
Colm <colm.conneely@paack.co>
2025-05-21 16:30:55 +0100

    invoicing

--- simple-pos-frontend/src/app/components/Invoice/Invoice.jsx
+++ simple-pos-frontend/src/app/components/Invoice/Invoice.jsx
@@ -1,22 +1,80 @@
 
+import '././index.css'
+
+const styles = {
+  margin : 'auto',
+  border : 'groove'
+}
+
 export default function Invoice() {
+  return (
+    <main className="m-5 p-5 xl:max-w-4xl md:max-w-xl lg:max-w-auto mx-auto bg-white">
+      <header className="border flex-column justify-center mb-5 xl:flex-row xl:justify-between">
+      {/* Header */}
+          <h2 className="font-bold uppercase tracking-wide text-4xl mb-3 text-center">O'Flaherty Logo</h2>
+          <div id="buttons-div" className="flex justify-center">
+                  <button className="btn btn-print m-1 underline">Print</button>
+                  <button className="btn btn-download m-1 underline">Download</button>
+                  <button className="btn btn-send m-1 underline">Send</button>
+          </div>
+      </header>
+      {/*End of Header */}
 
-    return (
-        <div id="invoice-container">
-            <div id="first-invoice-details-section" class="border flex flex-row justify-around">
-                <div id="client-details-div" className="border flex flex-col p-4 m-4"> 
-                        <div id='representatives-div' className="border p-4 m-2">Representatives of the late 'Deceased Name'</div>
-                        <div id='care-of-div' className="border p-4 m-2">C/O James Murphy</div>
-                        <div id='client-address-div' className="border p-4 m-2">Client address</div>
-                        <div id='client-address-div' className="border p-4 m-2">Client address</div>
+      {/* Company Details */}
+      <section className="flex flex-col items-end m-2 ">
+        <h2 className="text-xl uppercase">Company Name</h2>
+        <p>Company Address</p>
+      </section>
+      {/* End of Company Details */}
 
-                </div> 
-                <div id="logo-and-invNumber-div" className="border flex flex-col p-4 m-4">
-                    <div id="logo-div" className="border p-8 m-2">LOGO</div>
-                    <div id="inv-number-div" className="border p-4 m-2">Invoice Number: 23492</div>
-                    <div id="inv-date-div" className="border p-4 m-2">Invoice Date: 23 April 2025</div>
-                </div>
-            </div>
-        </div>
-    )
+      {/* Client Details */}
+      <section className="">
+        <h2>Client Name</h2>
+        <p>Client Address</p>
+      </section>
+      {/* End of Client Details */}
+
+
+      {/* Dates */}
+      <section className="flex flex-col items-end my-5 border-b">
+        <ul>
+          <li>Invoice Number</li>
+          <li>Invoice Date</li>
+          <li>Due Date</li>
+        </ul>
+      </section>
+      {/* ENd of Dates */}
+
+
+      {/* Table Here */}
+      <table className="border">
+        <tr className="border justify-between">
+          <td>Item</td>
+          <td>Description</td>
+          <td>Unit Price</td>
+          <td>Total</td>
+        </tr>
+      </table>
+
+      {/* Notes */}
+      <section>
+        {/* Textarea */}
+        <p>Notes..</p>
+      </section>
+      {/* End of Notes */}
+
+
+      <footer>
+        <ul>
+          <li>Company Name</li>
+          <li>Company Email</li>
+          <li>Company Phone</li>
+          <li>Bank Name</li>
+          <li>Bank Account</li>
+        </ul>
+      </footer>
+
+
+    </main>
+  );
 }
