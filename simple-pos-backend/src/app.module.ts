247b76a83c7e374314092568064517e38f87d22a
Colm <colm.conneely@paack.co>
2025-05-21 21:54:45 +0100

    updates

--- simple-pos-backend/src/app.module.ts
+++ simple-pos-backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FormTemplatesModule } from './form-templates/form-templates.module';
 import { FuneralsModule } from './funerals/funerals.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FormTemplatesModule, FuneralsModule],
+  imports: [FormTemplatesModule, FuneralsModule, MongooseModule.forRoot('mongodb+srv://colm:@Hellmans4167@cluster0.zi4dix8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')],
   controllers: [AppController],
   providers: [AppService],
 })
