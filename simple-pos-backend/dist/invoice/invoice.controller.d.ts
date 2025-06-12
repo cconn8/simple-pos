d00b577b445e2c49212c5db25bb441c734314082
Colm <colm.conneely@paack.co>
2025-06-12 20:44:00 +0100

    rough but functioning

--- simple-pos-backend/dist/invoice/invoice.controller.d.ts
+++ simple-pos-backend/dist/invoice/invoice.controller.d.ts
@@ -2,5 +2,5 @@ import { InvoiceService } from './invoice.service';
 export declare class InvoiceController {
     private readonly invoiceService;
     constructor(invoiceService: InvoiceService);
-    generateInvoice(id: string): Promise<any>;
+    generateInvoice(id: string, body: any): Promise<any>;
 }
