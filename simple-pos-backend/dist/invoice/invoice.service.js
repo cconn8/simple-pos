2f68726fedb27c28b331c0ab2e362697678ffe96
Colm <colm.conneely@paack.co>
2025-06-25 11:52:30 +0100

    template updates

--- simple-pos-backend/dist/invoice/invoice.service.js
+++ simple-pos-backend/dist/invoice/invoice.service.js
@@ -67,6 +67,9 @@ let InvoiceService = class InvoiceService {
         console.log('Products and services total = ', productsAndServicesTotal);
         console.log('Subtotal = ', subtotal);
         const { fromDate, toDate, invoiceNumber, misterMisses, clientName, addressLineOne, addressLineTwo, addressLineThree } = data.additionalInvoiceData;
+        const formattedFromDate = new Date(fromDate).toDateString();
+        const formattedToDate = new Date(toDate).toDateString();
+        console.log(`new dates are ${formattedFromDate} - ${formattedToDate}`);
         const templateData = {
             data,
             services,
@@ -76,7 +79,7 @@ let InvoiceService = class InvoiceService {
             disbursementsTotal,
             subtotal,
             serviceCharge,
-            fromDate, toDate, invoiceNumber, misterMisses, clientName, addressLineOne, addressLineTwo, addressLineThree
+            formattedFromDate, formattedToDate, invoiceNumber, misterMisses, clientName, addressLineOne, addressLineTwo, addressLineThree
         };
         const templatePath = path.join(process.cwd(), 'templates', 'invoice.template4.hbs');
         const source = fs.readFileSync(templatePath, 'utf8');
